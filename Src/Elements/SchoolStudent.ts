4147a7df385720be95e3c4b2a3e90195d88733fe
juanlll <juan.vargasva@gmail.com>
2022-08-22 21:51:05 -0500

    Update SchoolStudent.ts

--- Src/Elements/SchoolStudent.ts
+++ Src/Elements/SchoolStudent.ts
@@ -3,7 +3,7 @@ import { Visitor } from '../Visitors/Visitor';
 
 export class SchoolStudent extends StudentWithAccept {
   public weightFixed: number = 1;
-  accept<T>(visitor: Visitor<T>): T {
+  public accept<T>(visitor: Visitor<T>): T {
     return visitor.visitSchoolStudent(this);
   }
 }
