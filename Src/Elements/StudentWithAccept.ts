74599a3fefc65494ad2c04307ae359488e8669bd
juanlll <juan.vargasva@gmail.com>
2022-08-22 21:49:45 -0500

    Update StudentWithAccept.ts

--- Src/Elements/StudentWithAccept.ts
+++ Src/Elements/StudentWithAccept.ts
@@ -2,5 +2,5 @@ import { Student } from './Student';
 import { Visitor } from '../Visitors/Visitor';
 
 export abstract class StudentWithAccept extends Student {
-  abstract accept<T>(visitor: Visitor<T>): T;
+  public abstract accept<T>(visitor: Visitor<T>): T;
 }
